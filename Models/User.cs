e4ff486eb0fe65261648b3e25069cf042be50772
VDemchenko <VDemchenko@ASUSVIVOBOOK>
2024-04-24 01:50:23 +0300

    add Film, User + Models

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using ProjectFilm.Model;
+
+namespace ProjectFilm.Model
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string HashedPassword { get; set; }
+        public string Salt { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public IEnumerable<ChatMessage> Messages { get; set; }
+        public IEnumerable<Review> Reviews { get; set; }
+    }
+}
